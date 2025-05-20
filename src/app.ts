e3186985a0dc4fc0685a4473a3ad95edea276ca3
Isaac Ubani <isaacchimdi@gmail.com>
2025-05-20 15:32:06 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -29,11 +29,11 @@ class App {
 
   constructor(routes: Routes[]) {
     // this.connectToDatabase();
+    console.log( process.env.POSTGRES_HOST," ", process.env.POSTGRES_PORT, " " ,process.env.POSTGRES_USERNAME, "  " ,process.env.POSTGRES_PASSWORD," ", PORT)
+
     this.app = express();
     this.env = NODE_ENV || "development";
     this.port = process.env.PORT || 3000;
-
-    console.log( process.env.POSTGRES_HOST," ", process.env.POSTGRES_PORT, " " ,process.env.POSTGRES_USERNAME, "  " ,process.env.POSTGRES_PASSWORD," ", PORT)
     
     this.initializeMiddlewares();
     this.initializeRoutes(routes);
